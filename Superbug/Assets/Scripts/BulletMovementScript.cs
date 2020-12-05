1a3a3001d2e263ef81b882a97fbe646882a017d0
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-12-05 04:20:58 +0000

    Bullets firing on timer with particle effect and bounding box added

--- Superbug/Assets/Scripts/BulletMovementScript.cs
+++ Superbug/Assets/Scripts/BulletMovementScript.cs
@@ -2,30 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovementScript : MonoBehaviour
+public class BulletMovementScript : MonoBehaviour
 {
-	public GameObject enemy;
+	public GameObject bullet;
 
-    public float moveSpeed;
-    private Rigidbody2D enemyRigidbody2D;
+	public float moveSpeed;
+	private Rigidbody2D bulletRigidbody2D;
 
 	private Vector2 moveInput;
 	private Vector2 moveVelocity;
 
 	private void Start() {
-		enemyRigidbody2D = GetComponent<Rigidbody2D>();
+		bulletRigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
 	private void Update() {
 		moveInput = new Vector3(2f, 0f);
 		moveVelocity = moveInput * moveSpeed;
 
-		if (transform.position.x <= -12) {
-			GameObject.Destroy(enemy);
+		if (transform.position.x >= 15) {
+			GameObject.Destroy(bullet);
 		}
 	}
 
 	private void FixedUpdate() {
-		enemyRigidbody2D.velocity = moveVelocity;
+		bulletRigidbody2D.velocity = moveVelocity;
 	}
 }
---
+++ Superbug/Assets/Scripts/BulletSpawnerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawnerScript : MonoBehaviour {
+	public GameObject bulletSpawner;
+
+	public float moveSpeed;
+	private Rigidbody2D bulletSpawnerRigidbody2D;
+
+	private Vector2 moveInput;
+	private Vector2 moveVelocity;
+
+	public float spawnerMovementSpeed = 5;
+
+	private float spawnerTimer = 0.0f;
+
+	private void Start() {
+		bulletSpawnerRigidbody2D = GetComponent<Rigidbody2D>();
+
+		Instantiate(bulletSpawner, transform.position, Quaternion.identity);
+	}
+
+	private void Update() {
+
+
+		moveInput = new Vector2(0f, -0.5f);
+		moveVelocity = moveInput * moveSpeed;
+
+		moveInput = new Vector2(0f, 0.5f);
+		moveVelocity = moveInput * moveSpeed;
+
+	}
+
+		private void FixedUpdate() {
+
+			bulletSpawnerRigidbody2D.velocity = moveVelocity;
+
+			spawnerTimer += Time.deltaTime;
+
+			if (Input.GetKeyDown("space")) {
+				Instantiate(bulletSpawner, transform.position, Quaternion.identity);
+				spawnerTimer = 0;
+			}
+
+		}
+	}
