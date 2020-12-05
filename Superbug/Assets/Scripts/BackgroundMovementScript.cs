de3166fa3c8706f1a5ada200479b0910ba527949
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-12-05 15:37:52 +0000

    Parallax background added

---
+++ Superbug/Assets/Scripts/BackgroundMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovementScript : MonoBehaviour
+{
+	// Get game objects
+	public GameObject backgroundLayer;
+
+	// Movement speed and rigidbody2D
+	public float moveSpeed;
+	private Rigidbody2D backgroundRigidbody2D;
+
+	// Vectors for user input and setting velocity
+	private Vector2 moveInput;
+	private Vector2 moveVelocity;
+
+	// Get rigidbody2D at start
+	private void Start() {
+		backgroundRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Calculate background movement speed and direction
+	private void Update() {
+		moveInput = new Vector3(2f, 0f);
+		moveVelocity = moveInput * moveSpeed;
+
+		// Destory backgrounds when they are out of camera view
+		if (transform.position.x <= -25) {
+			GameObject.Destroy(backgroundLayer);
+		}
+	}
+
+	// Apply movement to the bullet
+	private void FixedUpdate() {
+		backgroundRigidbody2D.velocity = moveVelocity;
+	}
+}
---
+++ Superbug/Assets/Scripts/BackgroundSpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSpawnerScript : MonoBehaviour
+{
+    public GameObject backgroundLayer;
+
+    private float spawnerTimer = 0.0f;
+    public float timer = 0.0f;
+
+	// Instantiates one background layer on start
+	private void Start() {
+        Instantiate(backgroundLayer, transform.position, Quaternion.identity);
+    }
+
+	// Checks spawner timer against public timer in editor
+	private void FixedUpdate() {
+
+        spawnerTimer += Time.deltaTime;
+
+        if (spawnerTimer > timer) {
+            Instantiate(backgroundLayer, transform.position, Quaternion.identity);
+            spawnerTimer = 0;
+        }      
+    }
+}
