83f65e16f2ee8998b324060de4378a1053bccdcb
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-12-06 02:36:10 +0000

    Updated enemies

---
+++ Superbug/Assets/Scripts/EnemyHealthScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject enemy;
+
+    public float enemyHealth = 0.0f;
+    public float bulletDamage = 0.0f;
+
+    private bool enemyIsDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		
+	}
+}
