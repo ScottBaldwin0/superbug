49a4175cd347fd218125426026b182216745a3ff
ScottyBaldwin <71764565+ScottyBaldwin@users.noreply.github.com>
2020-12-06 23:37:41 +0000

    Revert "Revert "Main menu added""
    This reverts commit c67d7efdfcdf5ab5dc538850d6499834c35a2b8b.

---
+++ Superbug/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void playButton() {
+           SceneManager.LoadScene(3);
+       }
+   
+       public void optionsButton() {
+           SceneManager.LoadScene(1);
+       }
+   
+       public void quitButton() {
+           Application.Quit();
+       }
+}
